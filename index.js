fee3ac06e623cec9fffd0078aab06d672b59214c
Nelson Pecora <nelson@yoshokatana.com>
2015-03-30 17:07:00 -0400

    use consolidate

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict';
 var _ = require('lodash'),
-  embed = require('./lib/embed'),
-  engines = require('./lib/engines');
+  cons = require('consolidate'),
+  embed = require('./lib/embed');
 
 module.exports = function (instances) {
   // if instances are passed through, use them
   if (instances) {
     _.forOwn(instances, function (instance, name) {
       // add them to our engines
-      engines.set(name, instance);
+      cons.requires[name] = instance;
     });
   }
 
   // expose the renderer and the engines
   return {
     render: embed.render,
-    engines: engines.engines
+    engines: cons.engines
   };
 };
